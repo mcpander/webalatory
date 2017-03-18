ce6eec008b9d3ee779a2a203be03762f0f03ecfc
mcpander <timoshink@gmail.com>
2017-03-18 05:10:26 +0300

    works

--- webalatory.js
+++ webalatory.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello Webalatory!');
+    res.send('Hello Webalatory!it works!!');
 });
 
 app.listen(process.env.PORT || 80, function () {
