09724c9814072db7b1ee75e2bee71a7c54f161cc
mcpander <timoshink@gmail.com>
2017-05-04 16:21:42 +0300

    hair app date & time select

--- hair/js/main.js
+++ hair/js/main.js
@@ -2,12 +2,35 @@
  * Created by McPander on 01.05.2017.
  */
 'use strict'
+var events = JSON.parse(localStorage['events'] || {});
 window.addEventListener('load',function(){
     var userPic = document.querySelector('.user-pic img');
     var userPicContainer = document.querySelector('.user-pic');
     var userHaircut = document.querySelector('.user-haircut');
     var haircuts = document.querySelectorAll('.haircut img');
     var size = document.getElementById('size');
+    var date = document.getElementById('date');
+    var time = document.getElementById('time');
+    var btn = document.getElementById('addToList');
+    btn.onclick = function(){
+        if(date.value != '' && time.value != ''){
+            if(!events[+new Date(date.value)]){
+                events[+new Date(date.value)] = {};
+            }
+            events[+new Date(date.value)][time.value] = true;
+            localStorage['events'] = JSON.stringify(events);
+            var str = '';
+            for(var i = 1;i<=10;i++){
+                if(!events[+new Date(date.value)] || !events[+new Date(date.value)][i]){
+                    str += '<option value="'+i+'">'+(9+i)+':00</option>';
+                }
+            }
+            alert('Вы записанны на стрижку на '+new Date(date.value).toLocaleDateString()+' на'+(time.value+9)+':00!')
+            time.innerHTML = str;
+        }else{
+            alert('Выберите дату и время!')
+        }
+    }
     haircuts.forEach(function(h){
         h.addEventListener('click',function(e){
             userHaircut.innerHTML = '<img src="'+e.target.src+'"/>';
@@ -15,6 +38,17 @@ window.addEventListener('load',function(){
     });
     size.addEventListener('change',function(event){
         userHaircut.querySelector('img').style.width = (event.target.value*100)+'%';
+    });
+    date.addEventListener('change',function(event){
+        var str = '';
+        if(event.target.value != ''){
+            for(var i = 1;i<=10;i++){
+                if(!events[+new Date(event.target.value)] || !events[+new Date(event.target.value)][i]){
+                    str += '<option value="'+i+'">'+(9+i)+':00</option>';
+                }
+            }
+        }
+        time.innerHTML = str;
     });
     userPic.addEventListener('click',function(){
         var loader = document.createElement('input');
